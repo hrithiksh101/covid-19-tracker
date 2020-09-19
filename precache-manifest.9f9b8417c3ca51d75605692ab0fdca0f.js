26fd413b504d80510251beab012c0f2ef48c8c88
hrithiksh101 <hrithik.sh.13@gmail.com>
2020-09-19 10:04:14 +0530

    Updates

---
+++ precache-manifest.9f9b8417c3ca51d75605692ab0fdca0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5cbc7bcee855b18abce419e5f387a0",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "36abcbdac104f25b25fd",
+    "url": "/covid-19-tracker/static/css/main.baba6fa6.chunk.css"
+  },
+  {
+    "revision": "70863d45c2bb1f0b4df9",
+    "url": "/covid-19-tracker/static/js/2.b8958036.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.b8958036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36abcbdac104f25b25fd",
+    "url": "/covid-19-tracker/static/js/main.87ef0097.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
